cf4a78edaa7056339d8c938ff29c2aa9f2a458f5
alex90 <startsts@yandex.ru>
2017-09-04 08:13:28 -0300

    linux/nvidia almost done

--- miner.py
+++ miner.py
@@ -11,6 +11,7 @@ import os
 import platform
 import telnetlib
 import datetime
+import re
 import decimal
 
 
@@ -26,16 +27,32 @@ class gui():
     radeon = False
     old_cpu = False
     started_gpu = False
+    started_gpu_title = None
     started_cpu = False
+    started_cpu_title = None
     cpu_threads = 1
     cpu_coin = None
     gpu_coin = None
+
+    def on_destroy(self, widget=None, *data):
+
+        messagedialog = Gtk.MessageDialog(self.window, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.OK_CANCEL, "On close mining process will be stopped")
+        messagedialog.show_all()
+        result = messagedialog.run()
+        messagedialog.destroy()
+        if result == Gtk.ResponseType.CANCEL:
+            return True
+        self.kill_miner("gpu")
+        self.kill_miner("cpu")
+        Gtk.main_quit()
+        return False
 
     def __init__(self):
         self.dir = os.path.dirname(os.path.abspath(__file__))
         self.window = Gtk.Window()
         self.window.set_default_size(800, 600)
-        self.window.connect('delete-event', Gtk.main_quit)
+        self.window.set_title("CryptoHubMiner")
+        self.window.connect('delete-event', self.on_destroy)
 
         self.box = Gtk.VBox()
         self.window.add(self.box)
@@ -59,14 +76,20 @@ class gui():
         self.box_c1 = Gtk.VBox()
         self.hbox_status.pack_start(self.box_c1, False, True, 30)
 
-        self.label_title_cpu = Gtk.Label('Coin 1')
+        self.label_title_cpu = Gtk.Label('')
         self.box_c1.pack_start(self.label_title_cpu, True, True, 0)
 
-        self.label_hashrate_cpu = Gtk.Label('Hashrate 2')
+        self.label_hashrate_cpu = Gtk.Label('')
         self.box_c1.pack_start(self.label_hashrate_cpu, True, True, 0)
 
-        self.label_coin_cpu_bal_conf = Gtk.Label('Confirmed balance:')
-        self.box_c1.pack_start(self.label_coin_cpu_bal_conf, True, True, 0)
+        self.label_accepted_cpu = Gtk.Label('')
+        self.box_c1.pack_start(self.label_accepted_cpu, True, True, 0)
+
+        self.label_rejected_cpu = Gtk.Label('')
+        self.box_c1.pack_start(self.label_rejected_cpu, True, True, 0)
+
+        #self.label_coin_cpu_bal_conf = Gtk.Label('Confirmed balance:')
+        #self.box_c1.pack_start(self.label_coin_cpu_bal_conf, True, True, 0)
 
         self.fr2 = Gtk.Frame()
         self.hbox_status.pack_start(self.fr2, True, True, 10)
@@ -74,11 +97,17 @@ class gui():
         self.box_c2 = Gtk.VBox()
         self.hbox_status.pack_start(self.box_c2, False, True, 30)
 
-        self.label_title_gpu = Gtk.Label('Coin 2')
+        self.label_title_gpu = Gtk.Label('')
         self.box_c2.pack_start(self.label_title_gpu, True, True, 0)
 
-        self.label_hashrate_gpu = Gtk.Label('Hashrate 2')
+        self.label_hashrate_gpu = Gtk.Label('')
         self.box_c2.pack_start(self.label_hashrate_gpu, True, True, 0)
+
+        self.label_accepted_gpu = Gtk.Label('')
+        self.box_c2.pack_start(self.label_accepted_gpu, True, True, 0)
+
+        self.label_rejected_gpu = Gtk.Label('')
+        self.box_c2.pack_start(self.label_rejected_gpu, True, True, 0)
 
 
 
@@ -129,6 +158,29 @@ class gui():
                 self.old_cpu = True
 
 
+
+        self.label_cpu = Gtk.Label("Enter your CryptoHub user:")
+        self.box.pack_start(self.label_cpu, True, True, 20)
+
+        self.user_input_box = Gtk.HBox()
+        self.box.pack_start(self.user_input_box, True, True, 10)
+
+
+
+        self.user_input_frame = Gtk.Frame()
+        self.user_input_frame.get_style_context().add_class("inp")
+
+
+        self.user_input = Gtk.TextView()
+        self.user_input_box.pack_start(self.user_input_frame, True, True, 50)
+        self.user_input_frame.add(self.user_input)
+
+        self.gtk_style()
+
+
+
+
+
         if self.old_cpu:
             self.label_cpu = Gtk.Label("Your CPU is too old and doesn't support AES not AVX.")
             self.box.pack_start(self.label_cpu, True, True, 20)
@@ -142,7 +194,7 @@ class gui():
             self.box_nv.pack_start(self.cpuhbox, True, True, 10)
 
             self.combobox_cpu = Gtk.ComboBoxText()
-            self.combobox_cpu.append_text("Mining Q2C")
+            self.combobox_cpu.append_text("Q2C")
             self.cpuhbox.pack_start(self.combobox_cpu, True, True, 10)
 
             self.label_cpu_threads = Gtk.Label("Threads:")
@@ -207,57 +259,72 @@ class gui():
         Gtk.main()
         Gdk.threads_leave()
 
+    def open_log(self, file):
+        a = open(file, 'rb')
+        lines = a.readlines()
+        if lines:
+            last_line = lines[-1]
+            return last_line
 
-    def telnet_get(self, command, port):
-        if self.nvidia:
-            tn = telnetlib.Telnet("127.0.0.1", port)
-            tn.write(str(command + "\n").encode('ascii'))
+    def get_log(self, type="gpu"):
+        res_data_all = {
+            "hashrate": 0,
+            "accepted": 0,
+            "rejected": 0
+        }
+        last_line = str(self.open_log(type + "miner.txt"))
+        print(last_line)
+        if "[S/A/T]" in last_line:
+            # Alexis ccminer
+            print("Alexis ccminer")
+            els = last_line.split(",")
+            shares = str(els[0].split(":")[-1]).strip().split("/")
+            hs = str(els[2].split("/s")[0]).strip()
+            res_data_all["hashrate"] = hs + "/s"
+            res_data_all["accepted"] = int(shares[1])
+            res_data_all["rejected"] = int(shares[2]) - int(shares[1])
+        elif "Accepted" in last_line:
+            # common ccminer
+            print("common ccminer")
+            els = last_line.split("Accepted")[1].split(",")
+            shares = els[0].strip().split(" ")[1].split("/")
+            print(els, shares)
+            hs = els[2]
+            res_data_all["hashrate"] = hs
+            res_data_all["accepted"] = int(shares[0])
+            res_data_all["rejected"] = int(shares[1]) - int(shares[0])
 
-            res_data_all = []
-            rs = tn.read_all()
-            rs = str(rs)[2:].split("|")
-            for el in rs:
-                res = str(el).split(";")
-                res_data = {}
-                for r in res:
-                    parts = r.split("=")
-                    if len(parts)>1:
-                        k = parts[0]
-                        res_data[k] = parts[1]
-                res_data_all.append(res_data)
-            return res_data_all
+
+
+        return res_data_all
 
     def upd(self):
         if self.started_gpu:
             self.label_status.set_markup('<span size="xx-large" foreground="green">Running</span>')
             try:
-                res_data = self.telnet_get("summary", 4090)
-                self.label_hashrate.set_label(str(res_data["KHS"]))
-
-                try:
-                    res_data = self.telnet_get("scanlog", 4090)
-                    print(res_data)
-                except:
-                    pass
+                res_data = self.get_log("gpu")
+                self.label_title_gpu.set_markup('<span size="large" foreground="green">'+ str(self.started_gpu_title) +'</span>')
+                self.label_hashrate_gpu.set_label(str(res_data["hashrate"]))
+                if res_data["accepted"] > 0 or res_data["rejected"] > 0:
+                    self.label_accepted_gpu.set_label("Accepted:" + str(res_data["accepted"]))
+                    self.label_rejected_gpu.set_label("Rejected:" + str(res_data["rejected"]))
 
             except Exception as e:
                 print(e)
+        else:
+            self.label_title_gpu.set_markup('<span size="large" foreground="green"></span>')
+            self.label_hashrate_gpu.set_label("")
 
         if self.started_cpu:
             self.label_title_cpu.set_markup('<span size="large" foreground="green">Q2C</span>')
             try:
-                res_data = self.telnet_get("summary", 4091)
-                '''try:
-                    khs = decimal.Decimal(res_data[0]["KHS"])
-                except:
-                    khs = 0'''
-                khs = 0
-                if khs <= 0:
-                    res_data = self.telnet_get("threads", 4091)
-                    for el in res_data:
-                        if 'KHS' in el:
-                            khs += decimal.Decimal(el['KHS'].replace("|'",""))
-                self.label_hashrate_cpu.set_label(str(khs))
+                res_data = self.get_log("cpu")
+                self.label_title_cpu.set_markup('<span size="large" foreground="green">' + str(self.started_cpu_title) + '</span>')
+                self.label_hashrate_cpu.set_label(str(res_data["hashrate"]))
+                if res_data["accepted"] > 0 or res_data["rejected"] > 0:
+                    self.label_accepted_cpu.set_label("Accepted:" + str(res_data["accepted"]))
+                    self.label_rejected_cpu.set_label("Rejected:" + str(res_data["rejected"]))
+
             except Exception as e:
                 print(e)
         else:
@@ -275,49 +342,97 @@ class gui():
 
     def on_gpu_button_clicked(self, widget):
         key = self.combobox_gpu.get_active_text()
+        buf = self.user_input.get_buffer()
+        bs = buf.get_bounds()
+        user = buf.get_text(bs[0],bs[1],True)
+        if not user:
+            return
         pool = pools_gpu[key]
         print(pool)
         if self.nvidia:
-            prc = self.dir + "/ccminer/ccminer -a " + pool[1] + " -o stratum+tcp://" + pool[0] + " -u support@cryptohub.online -p x -b 127.0.0.1:4090"
+            prc = self.dir + "/ccminer/ccminer -a " + pool[1] + " -o stratum+tcp://" + pool[0] + " -u " + user + " -p x"
         elif self.radeon:
             print("to do")
+        else:
+            return
         if self.nvidia or self.radeon:
-            subprocess.call(prc + " &", shell=True)
+            subprocess.call(prc + " > gpuminer.txt &", shell=True)
+            #/bin/sh: 1: /home/alex90/PycharmProjects/cryptohubminer/ccminer/ccminer: not found
+            self.started_gpu_title = key
             self.started_gpu = True
 
         self.gpu_button.set_sensitive(False)
         self.gpu_button2.set_sensitive(True)
 
-    def on_gpu_button_clicked2(self, widget):
-        if self.nvidia:
-            prc_name = "ccminer"
-        elif self.radeon:
-            prc_name = "sgminer"
-        for proc in psutil.process_iter():
-            if proc.name() == prc_name:
-                proc.kill()
+    def kill_miner(self, type):
+        if type == "gpu":
+            if self.nvidia:
+                prc_name = "ccminer"
+            elif self.radeon:
+                prc_name = "sgminer"
+            else:
+                return
+            for proc in psutil.process_iter():
+                if proc.name() == prc_name:
+                    proc.kill()
+        if type == "cpu":
+            prc_name = "cpuminer"
+            for proc in psutil.process_iter():
+                if proc.name() == prc_name:
+                    proc.kill()
 
+    def on_gpu_button_clicked2(self, widget):
+        self.kill_miner("gpu")
         self.gpu_button.set_sensitive(True)
         self.gpu_button2.set_sensitive(False)
         self.started_gpu = False
 
     def on_cpu_button_clicked(self, widget):
+        buf = self.user_input.get_buffer()
+        bs = buf.get_bounds()
+        user = buf.get_text(bs[0], bs[1], True)
+        if not user:
+            return
         threads = self.combobox_threads.get_active_text()
-        prc = self.dir + "/cpuminer/cpuminer -a nist5 -o stratum+tcp://cryptohub.online:3032 -u support@cryptohub.online -p x -b 127.0.0.1:4091 -t " + str(threads)
-        subprocess.call(prc + " &", shell=True)
+        prc = self.dir + "/cpuminer/cpuminer -a qubit -o stratum+tcp://cryptohub.online:3042  -u " + user + " -p x  -t " + str(threads)
+        subprocess.call(prc + " > cpuminer.txt &", shell=True)
+        self.started_cpu_title = self.combobox_cpu.get_active_text()
         self.started_cpu = True
         self.cpu_button.set_sensitive(False)
         self.cpu_button2.set_sensitive(True)
 
-
     def on_cpu_button_clicked2(self, widget):
-        prc_name = "cpuminer"
-        for proc in psutil.process_iter():
-            if proc.name() == prc_name:
-                proc.kill()
+        self.kill_miner("cpu")
         self.cpu_button.set_sensitive(True)
         self.cpu_button2.set_sensitive(False)
         self.started_cpu = False
+
+    def gtk_style(self):
+            css = b"""
+    * {
+        transition-property: color, background-color, border-color, background-image, padding, border-width;
+        transition-duration: 0.6s;
+        font-size: 14px;
+    }
+
+    frame.inp {
+        border-style: solid;
+        border-width: 1px;
+        padding: 6px;
+        border-color: black;
+    }
+            """
+            style_provider = Gtk.CssProvider()
+            style_provider.load_from_data(css)
+
+            Gtk.StyleContext.add_provider_for_screen(
+                Gdk.Screen.get_default(),
+                style_provider,
+                Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+            )
+
+
+
 
 
 g = gui()
